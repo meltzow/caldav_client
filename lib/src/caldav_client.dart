250bb5b34c0bc98e9235b4a0f58a06468732cf54
Tyler Lin <tylerhlin@gmail.com>
2023-09-12 14:37:33 -0400

    Support incremental syncing

--- lib/src/caldav_client.dart
+++ lib/src/caldav_client.dart
@@ -12,7 +12,7 @@ class CalDavClient extends CalDavBase {
             connectionTimeout: connectionTimeout,
             headers: headers);
 
-  Future<CalResponse> getPrincipal() {
+  Future<CalResponse> getPrincipal({int depth = 0}) {
     final body = '''
     <d:propfind xmlns:d="DAV:">
       <d:prop>
@@ -20,10 +20,10 @@ class CalDavClient extends CalDavBase {
       </d:prop>
     </d:propfind>
     ''';
-    return propfind('', body, depth: 0);
+    return propfind('', body, depth: depth);
   }
 
-  Future<CalResponse> getCalendars(String path) {
+  Future<CalResponse> getCalendars(String path, {int depth = 1}) {
     final body = '''
     <d:propfind xmlns:d="DAV:" xmlns:cs="http://calendarserver.org/ns/" xmlns:c="urn:ietf:params:xml:ns:caldav">
       <d:prop>
@@ -34,33 +34,16 @@ class CalDavClient extends CalDavBase {
       </d:prop>
     </d:propfind>
     ''';
-    return propfind(path, body, depth: 1);
-  }
-
-  /// Get the display name and the ctag.
-  /// This ctag works like a change id.
-  /// Every time the ctag has changed, you know something in the calendar has changed too.
-  Future<CalResponse> initialSync(String path,
-      {int? depth, bool syncToken = true}) {
-    var syncTokenTag = '\n<d:sync-token />';
-    var body = '''
-    <d:propfind xmlns:d="DAV:" xmlns:cs="http://calendarserver.org/ns/">
-      <d:prop>
-      <d:displayname />
-      <cs:getctag />${syncToken ? syncTokenTag : ''}
-      </d:prop>
-    </d:propfind>
-    ''';
-
     return propfind(path, body, depth: depth);
   }
 
   /// This request will give us every object that's a VCALENDAR object, and its etag.
-  Future<CalResponse> getEvents(String path, {int? depth}) {
+  Future<CalResponse> getEvents(String path, {int depth = 1}) {
     var body = '''
     <c:calendar-query xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:caldav">
     <d:prop>
         <d:getetag />
+        <d:sync-token />
         <c:calendar-data>
           <c:expand start="19000101T000000Z" end="99991231T235959Z" />
         </c:calendar-data>
@@ -81,7 +64,7 @@ class CalDavClient extends CalDavBase {
   /// This request will give us every object that's a VCALENDAR object, and its etag in a given time range.
   Future<CalResponse> getEventsInTimeRange(
       String path, DateTime start, DateTime end,
-      {int? depth}) {
+      {int depth = 1}) {
     final normalizedStart = stringify(start);
     final normalizedEnd = stringify(end);
     var body = '''
@@ -108,18 +91,16 @@ class CalDavClient extends CalDavBase {
   /// Request the ctag again on the calendar. If the ctag did not change, you still
   /// have the latest copy.
   /// If it did change, you must request all the etags in the entire calendar again.
-  Future<CalResponse> getChanges(String path, {int? depth}) {
+  Future<CalResponse> getChanges(String path,
+      {String syncToken = '', int depth = 1}) {
     var body = '''
-    <c:calendar-query xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:caldav">
+    <d:sync-collection xmlns:d="DAV:">
+      <d:sync-token></d:sync-token>
+      <d:sync-level>1</d:sync-level>
       <d:prop>
-        <d:getetag />
+          <d:getetag />
       </d:prop>
-      <c:filter>
-        <c:comp-filter name="VCALENDAR">
-          <c:comp-filter name="VEVENT" />
-        </c:comp-filter>
-      </c:filter>
-    </c:calendar-query>
+    </d:sync-collection>
     ''';
 
     return report(path, body, depth: depth);
@@ -129,13 +110,8 @@ class CalDavClient extends CalDavBase {
   /// from within a collection, if the Request- URI is a collection, or to retrieve
   /// a specific calendar object resource, if the Request-URI is a calendar object
   /// resource.
-  Future<CalResponse> multiget(String path, List<String> files, {int? depth}) {
-    var links = '';
-
-    files.forEach((file) {
-      links += '<d:href>' + join(path, file) + '</d:href>\n';
-    });
-
+  Future<CalResponse> multiget(String path, List<String> hrefs,
+      {int depth = 1}) {
     var body = '''
     <c:calendar-multiget xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:caldav">
       <d:prop>
@@ -143,7 +119,7 @@ class CalDavClient extends CalDavBase {
         <c:calendar-data />
       </d:prop>
       ''' +
-        links +
+        hrefs.map((href) => '<d:href>$href</d:href>').join('\n') +
         '''
     </c:calendar-multiget>
     ''';
